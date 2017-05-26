53e6fbef4dce18de5e954bc8b68fee0129d9a8aa
mcmarius <m.c.marius@outlook.com>
2017-05-26 22:21:45 +0300

    Fixing stuff.

--- Views/PlayerView.cpp
+++ Views/PlayerView.cpp
@@ -4,7 +4,7 @@ extern double x, y, z;
 extern double lx, ly, lz;
 extern double angle;
 
-bool loadedPlayerTexture = false;
+//bool loadedPlayerTexture = false;
 GLuint faceTexture;//, rightSideTexture, leftSideTexture;
 
 void loadPlayerTexture(const std::string nume, GLuint &id) {
@@ -15,8 +15,8 @@ void loadPlayerTexture(const std::string nume, GLuint &id) {
     glBindTexture(GL_TEXTURE_2D, id);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);  // Set texture1 wrapping to GL_REPEAT
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);  // Set texture1 wrapping to GL_REPEAT
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -25,24 +25,24 @@ void loadPlayerTexture(const std::string nume, GLuint &id) {
 }
 
 void drawPlayer() {
-    if(!loadedPlayerTexture) {
+    /*if(!loadedPlayerTexture) {
         loadedPlayerTexture = true;
         loadPlayerTexture("Textures/player_texture.png", faceTexture);
 //        loadPlayerTexture("Textures/Player/right_ear.png", rightSideTexture);
 //        loadPlayerTexture("Textures/Player/left_ear.png", leftSideTexture);
-    }
+    }*/
     double zero = 0, coord = 0.2;
     glPushMatrix();
-    glTranslated(x +lx, std::fmax(y - 0.1, 0.5) + 0.05, z + lz);
+    glTranslated(x + lx, std::fmax(y - 0.1, 0.5), z + lz);
     glRotated(angle, 0, 1, 0);
-    glColor3d(1, 1, 1);
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, faceTexture);
+    glColor3d(0.5, 0.5, 1);
+//    glEnable(GL_TEXTURE_2D);
+//    glBindTexture(GL_TEXTURE_2D, faceTexture);
     glBegin(GL_QUADS);      // the face
-        glVertex3d(zero, zero, zero);  glTexCoord2d(0, 0);
-        glVertex3d(zero, coord, zero);   glTexCoord2d(0.115, 0);
-        glVertex3d(coord, coord, zero);    glTexCoord2d(0.115, 0.115);
-        glVertex3d(coord, zero, zero);   glTexCoord2d(0, 0.115);
+        glVertex3d(zero, zero, zero);//  glTexCoord2d(0, 0);
+        glVertex3d(zero, coord, zero);//   glTexCoord2d(0.115, 0);
+        glVertex3d(coord, coord, zero);//    glTexCoord2d(0.115, 0.115);
+        glVertex3d(coord, zero, zero);//   glTexCoord2d(0, 0.115);
     glEnd();
 //    glDisable(GL_TEXTURE_2D);
 //    glEnable(GL_TEXTURE_2D);
@@ -86,7 +86,4 @@ void drawPlayer() {
     glPopMatrix();
 
     glDisable(GL_TEXTURE_2D);
-    glColor3d(0, 0, 1);
-    glScaled(0.1, 0.1, 0.1);
-    glutSolidDodecahedron();    // obiect de test
 }
